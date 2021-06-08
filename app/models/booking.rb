783592ea63d61efe3baa63d23f910d5794b9ea96
Guillaume Negro <negro.guillaume@gmail.com>
2021-06-08 14:16:13 +0200

    Validate session and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :session
   belongs_to :user
+
+  validates :status, inclusion: {in: ["accept", "decline", "demand", "passed", "canceled"]}
 end
--- db/migrate/20210607142501_create_bookings.rb
+++ db/migrate/20210607142501_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.string :status
+      t.string :status, default: "demand"
       t.references :session, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
