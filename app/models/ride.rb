b1094e4657287bf6080634a2b74a05b205c03378
Guillaume Negro <negro.guillaume@gmail.com>
2021-06-09 15:28:55 +0200

    search by sport working

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,4 +7,13 @@ class Ride < ApplicationRecord
   validates :spot, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_sport,
+    associated_against: {
+      sport: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
