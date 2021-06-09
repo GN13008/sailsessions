471e6f94e805740f8eaa7c3a606608eb65f01885
Ibrahim Habi <ibrahim.habi@outlook.com>
2021-06-09 16:19:07 +0200

    Seeds & Validates & Default ride description changed

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -11,7 +11,7 @@ class RidesController < ApplicationController
   end
 
   def new
-    @ride = Ride.new 
+    @ride = Ride.new
   end
 
   def create
@@ -20,22 +20,13 @@ class RidesController < ApplicationController
       redirect_to @ride
     else
       render :new
-    end 
+    end
   end
-
-  def accept
-
-  end 
-
-  def decline
-
-  end 
 
   private
 
   def ride_params
-    params.require(:ride).permit(:from, :spot, :start_date, :end_date, :title, :description, :nb_of_place, :sport, :exchange_item)
+    params.require(:ride).permit(:from, :spot, :start_date, :end_date, :title, :description, :nb_of_place, :sport, :exchange_item, :date)
   end
 
-  
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,8 +3,5 @@ class Ride < ApplicationRecord
   belongs_to :sport
   has_many :bookings
 
-  validates :from, presence: true
-  validates :spot, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :from, :spot, :start_date, :end_date, :date, presence: true
 end
